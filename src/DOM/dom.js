91755a63f08980b9a467f587a418659a73882441
KirillGos <106958670+KirillGos@users.noreply.github.com>
2022-09-03 22:10:31 +0300

    added sidebar project adder

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -4,8 +4,73 @@ export const domObj = {
     today: document.querySelector('.today'),
     week: document.querySelector('.week'),
     form: document.querySelector('.form-sec'),
+    cancelProject: document.querySelector('.cancel-project'),
+    addProject: document.querySelector('.add-a-folder-sec'),
+    addProjectForm: document.querySelector('.project-name'),
+    addProjectCancelBtn: document.querySelector('.project-cancel-btn'),
+    addProjectAddBtn: document.querySelector('.project-add-btn'),
+    projectNameInput: document.querySelector('.project-name-input'),
+    projects: document.querySelector('.projects'),
+    projectsSec: document.querySelector('.to-do-list-sec'),
 }
-
+export const project = [];
 export function addEvent(element, event, action) {
     return element.addEventListener(event, action);
 } 
+export function createElement(element, className) {
+    const newElement = document.createElement(element);
+    newElement.classList.add(className);
+    return newElement;
+}
+export function cancel(e) {
+    if(e.className.includes('hidden')) {
+        e.remove('hidden');
+    } else {
+        e.classList.add('hidden');
+    }
+}
+
+export function addProject() {
+    if(domObj.addProjectForm.className.includes('hidden')) {
+        domObj.addProjectForm.classList.remove('hidden');
+        domObj.addProject.classList.add('hidden');
+        domObj.projectsSec.classList.add('hidden');
+    } 
+}
+
+export class createProjectObject {
+    constructor(name) {
+            this.name = name;
+    }
+
+}
+export function createProject() {
+      let projectName = domObj.projectNameInput.value;
+    if(projectName !== '') {
+
+        const newProject = createElement('div', 'project');
+        const projectIcon = createElement('img', 'project-icon');
+        projectIcon.setAttribute('src', '../src/assets/to-do-icon.svg');
+        newProject.appendChild(projectIcon);
+        let projectNameP = createElement('p', 'project-name-p');
+      
+        projectNameP.innerText = projectName;
+        newProject.appendChild(projectNameP);
+        domObj.projectsSec.appendChild(newProject);
+        newProject.classList.add('folder');
+        domObj.addProjectForm.classList.add('hidden');
+        domObj.projectNameInput.value = '';
+        domObj.addProject.classList.remove('hidden');
+        domObj.projectsSec.classList.remove('hidden');
+        let newObj = new createProjectObject(projectName);
+    } else {
+        alert('Enter text')
+    }
+
+}
+export function addProjectCancel() {
+    domObj.addProjectForm.classList.add('hidden');
+    domObj.projectNameInput.value = '';
+    domObj.addProject.classList.remove('hidden');
+    domObj.projectsSec.classList.remove('hidden');
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { domObj, addEvent } from './DOM/dom';
+import { domObj, addEvent, cancel, addProject, createProject, addProjectCancel, project } from './DOM/dom';
 import { inboxSec } from './modules/inbox';
 import { todaySec } from './modules/today'; 
 import { weekSec } from './modules/week';
 import './style/main.css';
-
 
 const display = domObj.display;
 const inbox = domObj.inbox;
@@ -15,3 +14,15 @@ addEvent(today, 'click', todaySec);
 addEvent(week, 'click', weekSec);
 
 const formSec = domObj.form;
+const cancelProject = domObj.cancelProject;
+addEvent(cancelProject, 'click', cancel);
+
+
+const addFolderSec = domObj.addProject;
+addFolderSec.addEventListener('click', addProject);
+
+const addProjectAddBtn = domObj.addProjectAddBtn;
+addProjectAddBtn.addEventListener('click', createProject);
+
+const addProjectCancelBtn = domObj.addProjectCancelBtn;
+addProjectCancelBtn.addEventListener('click', addProjectCancel); 
